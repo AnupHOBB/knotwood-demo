da7919e9ed80732201190f6803f53d460c552de1
Anup Bhandari <anup.b@HOBB.COM>
2022-12-01 11:33:43 +0530

    Added feature to provide entire front end files from backend

--- backend.js
+++ backend.js
@@ -8,13 +8,59 @@ http.createServer((req, res) =>
         res.setHeader("Access-Control-Allow-Headers","resource-type, resource-path");
         let type = req.headers["resource-type"];
         if (type == undefined || type == null)
-            res.end();
+            fetchFrontend(req, res);
         else if (type === "file-list")
             fetchDirectoryList(res);
         else if (type === "image")
             fetchImage(res, req.headers["resource-path"]);  
     }
 ).listen(8080);
+
+function fetchFrontend(req, res)
+{
+    let type = req.headers["sec-fetch-dest"];
+    const fs = require("fs");
+    if (type === "document")
+    {
+        res.writeHead(200, {"Content-type" : "text/html"});
+        fs.readFile("main.html", "utf-8", (err, data) =>
+            {
+                res.write(data);
+                res.end();
+            }
+        );
+    }
+    else if (type === "style")
+    {
+        res.writeHead(200, {"Content-type" : "text/css"});
+        fs.readFile("main_style.css", "utf-8", (err, data) =>
+            {
+                res.write(data);
+                res.end();
+            }
+        );
+    }
+    else if (type === "script")
+    {
+        res.writeHead(200, {"Content-type" : "text/javascript"});
+        fs.readFile("main.js", "utf-8", (err, data) =>
+            {
+                res.write(data);
+                res.end();
+            }
+        );
+    }
+    else if (type === "image")
+    {
+        res.writeHead(200, {"Content-type" : "image/png"});
+        fs.readFile("assets/house.png", (err, data) =>
+            {
+                res.write(data);
+                res.end();
+            }
+        );
+    }
+}
 
 function fetchDirectoryList(res)
 {
