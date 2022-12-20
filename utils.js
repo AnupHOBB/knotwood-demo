8ac8698a1b00079afdf1cdec5f6ffedc91c792bd
Anup Bhandari <Anup.b@HOBB.COM>
2022-12-20 13:34:08 +0530

    Refactored utils.js

--- utils.js
+++ utils.js
@@ -1,4 +1,4 @@
-function loadColors(htmlDocument, colors, clickEvent)
+export function loadColors(htmlDocument, colors, clickEvent)
 {
     let colorList = htmlDocument.getElementById("list-color")
     for (let i=0; i<colors.length; i++)
@@ -12,7 +12,7 @@ function loadColors(htmlDocument, colors, clickEvent)
     }
 }
 
-function requestServer(request, ...extra)
+export function requestServer(request, ...extra)
 {
     let reader = new XMLHttpRequest()
     reader.open(request.method, request.url)
@@ -27,13 +27,13 @@ function requestServer(request, ...extra)
     reader.send()
 }
 
-function toRGB(str)
+export function toRGB(str)
 {
     let match = str.match(/rgba?\((\d{1,3}), ?(\d{1,3}), ?(\d{1,3})\)?(?:, ?(\d(?:\.\d?))\))?/)
     return match ? {r:match[1], g:match[2], b:match[3]} : {r:0, g:0, b:0}
 }
 
-function duplicateImagedata(source)
+export function duplicateImagedata(source)
 {
     let sourceData = source.data
     let copyData = new Uint8ClampedArray(sourceData.length)
@@ -42,11 +42,9 @@ function duplicateImagedata(source)
     return new ImageData(copyData, source.width, source.height)
 }
 
-function multiplyColor(c1, c2)
+export function multiplyColor(c1, c2)
 {   
     c1 /= 255
     c2 /= 255
     return c1 * c2 * 255
 }
-
-export { loadColors, requestServer, toRGB, duplicateImagedata, multiplyColor }
